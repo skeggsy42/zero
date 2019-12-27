3763e4935812c7c73acf0e05932787068d225aaa
Phil <phil@skeggs.net>
2019-12-27 22:01:45 +0000

    modified:   views.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,15 +4,24 @@ from  bme280 import BME280
 from enviroplus import gas
 from subprocess import PIPE, Popen
 import yfinance
+try:
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
 
 # Create your views here.
+def get_light():
+        data = ltr559.get_lux()
+        return str(data)
+
 def get_cpu_temperature():
     	process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
     	output, error = process.communicate()
     	return float(output[output.index('=') + 1:output.rindex("'")])
 
 def get_temp():
-	factor = 0.8
+	factor = 1.54
 	cpu_temps = [get_cpu_temperature()] * 5
 	bme280 = BME280()
 	cpu_temp = get_cpu_temperature()
@@ -44,13 +53,19 @@ def get_oxidising():
 	data = data.oxidising /1000
 	return str(data)
 
+
 def get_quote(str):
 	quote=yfinance.Ticker(str).info["regularMarketPrice"]
 	return quote 
 
+def get_quote_bid(str):
+        quote=yfinance.Ticker(str).info["bid"]
+        return quote
+
 def index(request):
 	temp = get_temp()
 	humidity = get_humidity()
+	light = get_light()
 	pressure = get_pressure()
 	reducing = get_reducing()
 	oxidising = get_oxidising()
@@ -60,22 +75,59 @@ def index(request):
 			    "Humidity="+humidity+" %"+"<BR>"+
 			    "Reducing="+reducing+" kO"+"<BR>"+
 			    "Oxidising="+oxidising+" kO"+"<BR>"+
-                            "<a href=\"http://192.168.1.39:8080/webapp/quotes/\">Quotes</a><BR></p>")
+                            "Light="+light+" Lux"+"<BR>"+
+			    "<a href=\"http://192.168.1.39:8080/webapp/quotes/\">Quotes</a><BR></p>")
 
 def quotes(request):
+        amzn = get_quote("AMZN")
         apple = get_quote("AAPL")
+        aviva  = get_quote("AV.L")
         bats = get_quote("BATS.L")
         brk = get_quote("BRK.L")
+        brkbid = get_quote_bid("BRK.L")
+        brk_b = get_quote("BRK-B")
+        bt = get_quote("BT-A.L")
         dis = get_quote("DIS")
         ko = get_quote("KO")
+        isrg = get_quote("ISRG")
         msft = get_quote("MSFT")
         wmt = get_quote("WMT")
+        jch = get_quote("JCH.L")
+        ibm = get_quote("IBM")
+        jnj = get_quote("JNJ")
+        tem = get_quote("TEM.L")
+        tsco = get_quote("TSCO.L")
+        googl = get_quote("GOOGL")
+        goog = get_quote("GOOG")
+        sse = get_quote("SSE.L")
+        gsk = get_quote("GSK.L")
+        hsba = get_quote("HSBA.L")
+        rbs = get_quote("RBS.L")
+        tmpl = get_quote("TMPL.L")
+        pg = get_quote("PG")
 
-        return HttpResponse("<p>Apple="+str(apple)+"<BR>"+
-                            "British American Tobacco="+str(bats)+"<BR>"+
-                            "Brooks="+str(brk)+"<BR>"+
-                            "Disney="+str(dis)+"<BR>"+
-                            "Coca Cola="+str(ko)+"<BR>"+
-                            "Microsoft="+str(msft)+"<BR>"+
-                            "Walmart="+str(wmt)+"<BR>"+
-                            "<a href=\"http://192.168.1.39:8080/webapp/\">Readings</a><BR></p>")
+        return HttpResponse("<p><b>Apple="+str(apple)+"</b><BR>"+
+                                "Amazon="+str(amzn)+"<BR>"+
+                                "Aviva="+str(aviva)+"<BR>"+
+                          	"British American Tobacco="+str(bats)+"<BR>"+
+                            	"Brooks="+str(brk)+"<BR>"+
+                            	"<b>Brooks Bid="+str(brkbid)+"</b><BR>"+
+                                "<b>Berkshire B="+str(brk_b)+"</b><BR>"+
+                                "BT="+str(bt)+"<BR>"+
+                                "Disney="+str(dis)+"<BR>"+
+                                "ISRG="+str(isrg)+"<BR>"+
+                                "JCH="+str(jch)+"<BR>"+
+				"Coca Cola="+str(ko)+"<BR>"+
+				"Goog="+str(goog)+"<BR>"+
+                                "Googl="+str(googl)+"<BR>"+
+                                "GSK="+str(gsk)+"<BR>"+
+                                "HSBC="+str(hsba)+"<BR>"+
+                                "JNJ="+str(jnj)+"<BR>"+
+                                "<b>Microsoft="+str(msft)+"</b><BR>"+
+                                "PG="+str(pg)+"<BR>"+
+                                "RBS="+str(rbs)+"<BR>"+
+                                "SSE="+str(sse)+"<BR>"+
+                                "TEM="+str(tem)+"<BR>"+
+                                "Tesco="+str(tsco)+"<BR>"+
+                                "Walmart="+str(wmt)+"<BR>"+
+                                "<a href=\"http://192.168.1.39:8080/webapp/\">Readings</a><BR></p>")
